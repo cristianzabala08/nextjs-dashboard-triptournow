ee4bbd035e9031a1556ffbc58a874d86b79d0950
czabala98 <98676841+czabalaAmero@users.noreply.github.com>
2025-03-29 10:44:50 -0400

    done

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -33,16 +35,16 @@ import {
   faLinkedinIn,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons'
-import React from 'react'
+import React, { useEffect } from 'react'
 import UserChart from '@/components/Page/Dashboard/UserChart'
 import IncomeChart from '@/components/Page/Dashboard/IncomeChart'
 import ConversionChart from '@/components/Page/Dashboard/ConversionChart'
 import SessionChart from '@/components/Page/Dashboard/SessionChart'
 import TrafficChart from '@/components/Page/Dashboard/TrafficChart'
-import { getDictionary } from '@/locales/dictionary'
+import useDictionary from '@/locales/dictionary-hook'
 
-export default async function Page() {
-  const dict = await getDictionary()
+export default  function Page() {
+  const dict =  useDictionary();
 
 
   return (
